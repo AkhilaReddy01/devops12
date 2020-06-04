7d7a331474364d472302660e5db80d0d427d9440
Akhila <akhilareddy0304@gmail.com>
2020-06-04 20:36:34 +0530

    check in code

--- BasicMath/BasicMathTest/UnitTest1.cs
+++ BasicMath/BasicMathTest/UnitTest1.cs
@@ -1,15 +1,15 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using BasicMath;
+using NUnit.Framework;
 
 namespace BasicMathTest
 {
  
-    [TestClass]
+    [TestFixture]
     public class UnitTest1
     {
  
-        [TestMethod]
+        [Test]
         public void Test_AddMethod()
         {
             BasicMaths bm = new BasicMaths();
@@ -17,7 +17,7 @@ namespace BasicMathTest
             Assert.AreEqual(res, 20);
         }
 
-        [TestMethod]
+        [Test]
         public void Test_SubstractMethod()
         {
             BasicMaths bm = new BasicMaths();
@@ -25,7 +25,7 @@ namespace BasicMathTest
             Assert.AreEqual(res, 0);
         }
 
-        [TestMethod]
+        [Test]
         public void Test_DivideMethod()
         {
             BasicMaths bm = new BasicMaths();
@@ -33,7 +33,7 @@ namespace BasicMathTest
             Assert.AreEqual(res, 2);
         }
 
-        [TestMethod]
+        [Test]
         public void Test_MultiplyMethod()
         {
             BasicMaths bm = new BasicMaths();
